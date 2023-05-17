6098236956f71785518a747bec76687c1c52b16d
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-05-18 02:20:17 +0600

    jungle jamboree server initial work is complete and added mongodb and get all toys data

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+
+const cors = require('cors');
+const app = express()
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+
+// jungle-jamboree
+// 3f5j5U4V9cG05M5D
+
+// middleWear
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9xgdj4e.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("jungleJamboree");
+    const allToysCollection = database.collection("allToysCollection");
+
+
+
+    app.get('/allToys',async(req,res)=>{
+        const result = await allToysCollection.find().toArray()
+        res.send(result)
+    })
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('This Is Jungle Jamboree server')
+})
+
+app.listen(port, () => {
+    console.log(`this server running on ${port} port`)
+
+})
