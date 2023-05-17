1d8c90d41df652410a19fc3f1cafbc39615daa1d
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-05-18 03:13:08 +0600

    created post server for add a toys

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ require('dotenv').config()
 
 const port = process.env.PORT || 5000
 
-// jungle-jamboree
-// 3f5j5U4V9cG05M5D
 
 // middleWear
 
@@ -15,48 +13,51 @@ app.use(cors())
 app.use(express.json())
 
 
-
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.9xgdj4e.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const database = client.db("jungleJamboree");
-    const allToysCollection = database.collection("allToysCollection");
-
-
-
-    app.get('/allToys',async(req,res)=>{
-        const result = await allToysCollection.find().toArray()
-        res.send(result)
-    })
+        const database = client.db("jungleJamboree");
+        const allToysCollection = database.collection("allToysCollection");
 
 
 
+        app.get('/allToys', async (req, res) => {
+            const result = await allToysCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        app.post('/addToys', async (req, res) => {
+            const body = req.body
+            //  console.log(body)
+            const result = await allToysCollection.insertOne(body)
+            res.send(result)
+        })
 
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
